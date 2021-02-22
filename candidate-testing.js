035a594e3fad1bd6286536b147244583000293df
zjf811 <zfleming811@gmail.com>
2021-02-22 02:48:19 +0000

    Updated correctAnswer variable to string

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,7 +6,7 @@ const input = require('readline-sync');
 let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "";
-let correctAnswer;
+let correctAnswer = "";
 let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters = 5 kilometers. ", "(5+3)/2*10=? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
